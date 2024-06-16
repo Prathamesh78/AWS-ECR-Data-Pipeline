c6c1223b8d826edc1b4a3aa11916ca42b3625016
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-06-17 02:20:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,71 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"  
 }
+
+resource "aws_s3_bucket" "bucket"{
+  bucket = "prathameshs3bucket7810"
+
+  tags = {
+    Name = "My Bucket"
+  }
+}
+
+resource "aws_s3_object" "file" {
+  bucket = aws_s3_bucket.bucket.id
+  key = "customers.csv"
+  source = "customers.csv"
+}
+
+resource "aws_db_instance" "myrds" {
+    allocated_storage   = var.dbstorage
+   storage_type        = "gp2"
+   identifier          = "rdstf"
+   engine              = "mysql"
+   engine_version      = "8.0.35"
+   instance_class      = "db.t3.micro"
+   username            = "admin"
+   password            = "Passw0rd!7810"
+   publicly_accessible = true
+   skip_final_snapshot = true
+
+   tags = {
+     Name = "MyRDS"
+   }
+ }
 
 resource "aws_ecr_repository" "repo" {
-  name = "AWS-ECR-Data-Pipeline"
+  name = "s3-to-rds"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
-resource "aws_lambda_function" "function" {
-  function_name = "aws-data-pipeline-function"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "app.main"
-  image_uri     = "891377100011.dkr.ecr.us-east-1.amazonaws.com/aws-data-pipeline:latest"
+output "s3_bucket_name" {
+  value = aws_s3_bucket.bucket.bucket
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+output "rds_endpoint" {
+  value = aws_db_instance.myrds.endpoint
 }
 
-resource "aws_iam_role_policy_attachment" "policy-attachment" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+output "rds_db_name" {
+  value = aws_db_instance.myrds.db_name
+}
+
+output "rds_username" {
+  value = aws_db_instance.myrds.username
+}
+
+output "rds_password" {
+  value = aws_db_instance.myrds.password
+  sensitive = true
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.repo.repository_url
+}
+
+variable "dbstorage" {
+  default = 20
 }
