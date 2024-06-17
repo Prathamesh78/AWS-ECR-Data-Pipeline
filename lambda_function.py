5989067c8f8c9b3148be9bb3da3521325bcb1358
Prathamesh Sawant <104883046+Prathamesh78@users.noreply.github.com>
2024-06-17 10:05:30 +0530

    Delete lambda_function.py

--- lambda_function.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def lambda_handler(event, context):
-    result = subprocess.run(['python', 's3_to_rds.py'], capture_output=True, text=True)
-    return {
-        'statusCode': 200,
-        'body': result.stdout
-    }
